db1346e79cb421e654841e732516cf3d210c392f
Payam Zahedi <pzahedi1995@gmail.com>
2024-01-16 16:47:20 +0100

    add go router

---
+++ lib/src/core/presentation/routing/router.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(routes: []);
+});
