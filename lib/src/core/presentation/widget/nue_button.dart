7da15bc7ba67620611449a53eabae66feff6e133
Payam Zahedi <pzahedi1995@gmail.com>
2024-01-24 11:41:52 +0100

    add pressed state to button

--- lib/src/core/presentation/widget/nue_button.dart
+++ lib/src/core/presentation/widget/nue_button.dart
@@ -1,108 +1,139 @@
-
 import 'package:flutter/material.dart';
 
-class NueButton extends StatelessWidget {
+class NueButton extends StatefulWidget {
   const NueButton({
     super.key,
     required this.size,
-    this.child,
     this.color,
     this.gradient,
+    this.onPressed,
+    this.child,
   }) : assert(color != null || gradient != null);
 
   final double size;
-  final Widget? child;
   final Color? color;
   final List<Color>? gradient;
+  final VoidCallback? onPressed;
+  final Widget? child;
+
+  @override
+  State<NueButton> createState() => _NueButtonState();
+}
+
+class _NueButtonState extends State<NueButton> {
+  final ValueNotifier<bool> _isPressed = ValueNotifier<bool>(false);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: size,
-      width: size,
-      padding: const EdgeInsets.all(6.5),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: Colors.black12,
-          width: 1,
-        ),
-        gradient: const LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xff2B2F33),
-            Color(0xff101113),
-          ],
-        ),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black54,
-            offset: Offset(18, 18),
-            blurRadius: 36,
-            spreadRadius: 0,
-          ),
-          BoxShadow(
-            color: Colors.white10,
-            offset: Offset(-18, -18),
-            blurRadius: 36,
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: Stack(
-        children: [
-          Container(
+    return GestureDetector(
+      onTap: widget.onPressed,
+      onTapDown: (details) {
+        _isPressed.value = true;
+      },
+      onTapUp: (details) {
+        _isPressed.value = false;
+      },
+      onTapCancel: () {
+        _isPressed.value = false;
+      },
+      child: ValueListenableBuilder(
+        valueListenable: _isPressed,
+        builder: (context, isPressed, child) {
+          print('isPressed: $isPressed');
+
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 50),
+            height: widget.size,
+            width: widget.size,
+            padding: const EdgeInsets.all(6.5),
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: color,
-              gradient: (gradient?.isEmpty ?? true)
-                  ? null
-                  : LinearGradient(
-                      begin: const Alignment(-0.6, -0.6),
-                      end: Alignment.bottomRight,
-                      colors: gradient!,
-                    ),
-            ),
-          ),
-          Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
+              border: Border.all(
+                color: Colors.black12,
+                width: 1,
+              ),
+              gradient: const LinearGradient(
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
                 colors: [
-                  Colors.white12,
-                  Colors.black12,
+                  Color(0xff2B2F33),
+                  Color(0xff101113),
                 ],
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  offset: Offset(6, 6),
-                  blurRadius: 12,
-                  spreadRadius: 0,
-                ),
-              ],
+              boxShadow: !isPressed
+                  ? const [
+                      BoxShadow(
+                        color: Colors.black54,
+                        offset: Offset(18, 18),
+                        blurRadius: 36,
+                        spreadRadius: 0,
+                      ),
+                      BoxShadow(
+                        color: Colors.white10,
+                        offset: Offset(-18, -18),
+                        blurRadius: 36,
+                        spreadRadius: 0,
+                      ),
+                    ]
+                  : null,
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: color,
-              gradient: (gradient?.isEmpty ?? true)
-                  ? null
-                  : LinearGradient(
-                      begin: Alignment.bottomRight,
-                      end: const Alignment(-0.6, -0.6),
-                      colors: gradient!,
-                    ),
-            ),
-            alignment: Alignment.center,
             child: child,
-          ),
-        ],
+          );
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: widget.color,
+                gradient: (widget.gradient?.isEmpty ?? true)
+                    ? null
+                    : LinearGradient(
+                        begin: const Alignment(-0.6, -0.6),
+                        end: Alignment.bottomRight,
+                        colors: widget.gradient!,
+                      ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.white12,
+                    Colors.black12,
+                  ],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(6, 6),
+                    blurRadius: 12,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: widget.color,
+                gradient: (widget.gradient?.isEmpty ?? true)
+                    ? null
+                    : LinearGradient(
+                        begin: Alignment.bottomRight,
+                        end: const Alignment(-0.6, -0.6),
+                        colors: widget.gradient!,
+                      ),
+              ),
+              alignment: Alignment.center,
+              child: widget.child,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/features/idle/presentation/page/idle_page.dart
+++ lib/src/features/idle/presentation/page/idle_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:m_and_m/src/core/presentation/provider/season_control_provider.dart';
+import 'package:m_and_m/src/core/presentation/widget/nue_button.dart';
 
 class IdlePage extends ConsumerWidget {
   const IdlePage({super.key});
@@ -17,11 +18,14 @@ class IdlePage extends ConsumerWidget {
           children: [
             const Text('Tap to start the season!'),
             const SizedBox(height: 16),
-            ElevatedButton(
-                onPressed: () {
-                  ref.read(seasonControlProvider.notifier).startSeason();
-                },
-                child: const Text('Start Season')),
+            NueButton(
+              size: 200,
+              color: const Color(0xff24272C),
+              onPressed: () {
+                ref.read(seasonControlProvider.notifier).startSeason();
+              },
+              child: const Text('Start Season'),
+            ),
           ],
         ),
       ),
