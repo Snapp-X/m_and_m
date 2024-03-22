3ef5d5ffa7193d0a9752495789d6aa9910eed4c0
Moritz Theis <moritz@snappx.io>
2024-03-22 09:27:44 +0100

    re-enable NueButton

--- lib/src/features/idle/presentation/page/idle_page.dart
+++ lib/src/features/idle/presentation/page/idle_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:m_and_m/src/core/presentation/provider/season_control_provider.dart';
-// import 'package:m_and_m/src/core/presentation/widget/nue_button.dart';
+import 'package:m_and_m/src/core/presentation/widget/nue_button.dart';
 import 'package:m_and_m/src/features/idle/presentation/widget/background_animation.dart';
 // import 'package:m_and_m/src/features/idle/presentation/widget/background_animation.dart';
 
@@ -37,57 +37,57 @@ class StartButton extends ConsumerWidget {
     return SizedBox(
       width: 200,
       height: 200,
-      child: FloatingActionButton(
+      child:
+          // FloatingActionButton(
+          //   onPressed: () {
+          //     ref.read(seasonControlProvider.notifier).startSeason();
+          //   },
+          //   child:
+          //       const Text('Start'), // You can replace this with your desired icon
+          //   backgroundColor:
+          //       const Color(0xffFDDD02), // Set the background color to yellow
+          //   shape: RoundedRectangleBorder(
+          //     borderRadius:
+          //         BorderRadius.circular(200.0), // This creates a circular shape
+          //   ),
+
+          NueButton(
+        size: 200,
+        color: const Color(0xffFDDD02),
+        borderGradient: const [
+          Colors.white12,
+          Color(0xffFDCD08),
+        ],
+        holderGradient: const [
+          Color(0xffFDCD08),
+          Color(0xffFEDD02),
+        ],
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.05),
+            offset: const Offset(18, 18),
+            blurRadius: 36,
+            spreadRadius: 0,
+          ),
+          const BoxShadow(
+            color: Colors.white24,
+            offset: Offset(-18, -18),
+            blurRadius: 36,
+            spreadRadius: 0,
+          ),
+        ],
+        innerShadow: const [],
         onPressed: () {
           ref.read(seasonControlProvider.notifier).startSeason();
         },
-        child:
-            const Text('Start'), // You can replace this with your desired icon
-        backgroundColor:
-            const Color(0xffFDDD02), // Set the background color to yellow
-        shape: RoundedRectangleBorder(
-          borderRadius:
-              BorderRadius.circular(200.0), // This creates a circular shape
+        child: const Text(
+          'Start',
+          style: TextStyle(
+            color: Colors.white54,
+            fontSize: 36,
+            fontWeight: FontWeight.w700,
+          ),
         ),
-
-        // NueButton(
-        //   size: 200,
-        //   color: const Color(0xffFDDD02),
-        //   borderGradient: const [
-        //     Colors.white12,
-        //     Color(0xffFDCD08),
-        //   ],
-        //   holderGradient: const [
-        //     Color(0xffFDCD08),
-        //     Color(0xffFEDD02),
-        //   ],
-        //   boxShadow: [
-        //     BoxShadow(
-        //       color: Colors.black.withOpacity(.05),
-        //       offset: const Offset(18, 18),
-        //       blurRadius: 36,
-        //       spreadRadius: 0,
-        //     ),
-        //     const BoxShadow(
-        //       color: Colors.white24,
-        //       offset: Offset(-18, -18),
-        //       blurRadius: 36,
-        //       spreadRadius: 0,
-        //     ),
-        //   ],
-        //   innerShadow: const [],
-        //   onPressed: () {
-        //     ref.read(seasonControlProvider.notifier).startSeason();
-        //   },
-        //   child: const Text(
-        //     'Start',
-        //     style: TextStyle(
-        //       color: Colors.white54,
-        //       fontSize: 36,
-        //       fontWeight: FontWeight.w700,
-        //     ),
-        //   ),
-        // )
       ),
     );
   }
